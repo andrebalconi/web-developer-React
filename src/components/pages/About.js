cdd38d24de4bbc3dc02e28910f2bffcd3573ddd0
andrebalconi <andrebalconi@hotmail.com>
2024-01-27 21:12:42 -0300

    Update About Page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,18 +1,18 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-import { useScrolltoTop } from "hooks/scroll";
+import { useScrolltoTop } from 'hooks/scroll'
 
-import Hero from "components/molecules/Hero";
-import Heading from "components/atoms/Heading";
-import Section from "components/molecules/Section";
-import Grid from "components/atoms/Grid";
-import Footer from "components/organisms/Footer";
+import Hero from 'components/molecules/Hero'
+import Heading from 'components/atoms/Heading'
+import Section from 'components/molecules/Section'
+import Grid from 'components/atoms/Grid'
+import Footer from 'components/organisms/Footer'
 
-import BreadCrumb from "components/atoms/BreadCrumb";
+import BreadCrumb from 'components/atoms/BreadCrumb'
 
-import HeroImage from "assets/AboutMe4.jpg";
-import AboutImage from "draws/Website";
+import HeroImage from 'assets/AboutMe4.jpg'
+import AboutImage from 'draws/Website'
 
 const ImageContainer = styled.div`
   svg {
@@ -21,10 +21,10 @@ const ImageContainer = styled.div`
     max-width: 380px;
     color: ${(props) => props.theme.colors.primary.main};
   }
-`;
+`
 
 const About = () => {
-  useScrolltoTop();
+  useScrolltoTop()
 
   return (
     <>
@@ -33,38 +33,54 @@ const About = () => {
           <h1>Andre Balconi</h1>
         </Heading>
         <BreadCrumb
-          items={[{ label: "Home", link: "/" }, { label: "About" }]}
+          items={[{ label: 'Home', link: '/' }, { label: 'About' }]}
         />
       </Hero>
       <Section>
         <Grid md={2}>
           <div>
-            <p>
-              My name is André, I'm from Brazil.
-            </p>
+            <p>My name is André, I'm from Brazil.</p>
             <p>
               My experience as a developer started in 2006 in the largest
               private bank in the country working in the automation od processes
               in the Credit Card operation.
             </p>
             <p>
-              In the last 6 years I have been working in modernization and
-              website maintenance projects at Mercer Human Resource Consulting
-              present in 44 countries and with operations in more than 130
-              countries.
-            </p>
-            <p>
               I have passion for my work and a great desire to share knowledge,
               which is the best way to acquire more!
             </p>
             <p>
-              Career development in a large financial company focused on Systems
-              Analysis and Process Automation. Experience with process mapping
-              in complex environments, Database for generating Reports and
-              Performance Indicators. I have been working in the last few years
-              with software development, mainly with web application development
-              using C #, ASP.NET webforms, MVC, HTML, CSS, Jquery, Bootstrap,
-              Angular JS, SQL Server and Methodology Agile (Scrum, XP, etc.).
+              With over 17 years of experience, I specialize in the development
+              and enhancement of cutting-edge web tools, coupled with a keen
+              focus on monitoring IT projects through collaborative efforts with
+              Software Factories. My expertise lies predominantly in the dynamic
+              realm of the financial market, spanning the banking sector and,
+              more recently, the private pension domain for the past seven
+              years.
+            </p>
+            <p>
+              Throughout my career, I have championed Object-Oriented
+              Programming (OOP) principles, consistently applying the SOLID
+              principles, and embraced agile project management methodologies
+              such as Scrum and XP. This approach has not only streamlined
+              project workflows but has also catalyzed a culture of continuous
+              improvement and adaptability.
+            </p>
+            <p>
+              My proficiency extends across a diverse array of technologies,
+              encompassing the .NET Framework, ASP.NET, C#, JavaScript, and
+              popular frontend frameworks like Angular and React. My version
+              control mastery includes TFS and GIT, and I possess in-depth
+              knowledge of database management utilizing Microsoft S2L Server.
+            </p>
+            <p>
+              Adept at both independent project management and collaborative
+              teamwork, I offer a versatile skill set to the table, ensuring not
+              only the seamless development of web systems but also their
+              rigorous testing and efficient maintenance. My commitment to
+              staying at the forefront of technological advancements enables me
+              to navigate the ever-evolving landscape of IT with agility and
+              innovation.
             </p>
           </div>
           <div>
@@ -83,7 +99,7 @@ const About = () => {
 
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default About;
+export default About
